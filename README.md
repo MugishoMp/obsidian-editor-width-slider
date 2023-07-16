797f95f3025e0521b29b5d39f47d264b58f461ab
mugisho <mugishompozi@gmail.com>
2023-07-16 17:25:20 +0200

    first version of plugin done

--- README.md
+++ README.md
@@ -1,95 +1,45 @@
-# Obsidian Sample Plugin
+# Obsidian Line Width Slider Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+![Plugin Logo](./plugin-logo.png) <!-- Replace with your plugin logo -->
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+> A plugin for Obsidian that allows you to adjust the editor line width using a simple slider in the status bar.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+## Features
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- Adjust the line width of the editor using a slider in the status bar.
+- Increase or decrease the line width to customize your editing experience.
+- Simple and intuitive interface for easy usage.
 
-## First time developing plugins?
+## Demo
 
-Quick starting guide for new plugin devs:
+![Demo GIF](./demo.gif) <!-- Replace with the path to your demo GIF -->
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+> Here is a brief demo showcasing the basic functionality of the Line Width Slider plugin.
 
-## Releasing new releases
+## Installation
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+1. Download the latest release of the plugin from the [Releases](https://github.com/your-username/obsidian-line-width-slider-plugin/releases) page.
+2. Extract the plugin folder from the downloaded ZIP file.
+3. Copy the plugin folder into your Obsidian vault's `.obsidian/plugins` directory.
+4. Launch Obsidian and open the settings.
+5. Navigate to the "Community Plugins" tab and enable the "Line Width Slider" plugin.
+6. Enjoy using the Line Width Slider in the status bar to adjust the editor line width.
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+## Usage
 
-## Adding your plugin to the community plugin list
+1. Once the plugin is enabled, you will see a slider in the status bar.
+2. Drag the slider to the left or right to increase or decrease the editor line width.
+3. The changes will be applied to the editor in real-time.
 
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+## Feedback and Support
 
-## How to use
+If you encounter any issues or have suggestions for improvements, please create a new [issue](https://github.com/your-username/obsidian-line-width-slider-plugin/issues) on the GitHub repository.
 
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
+## Contributing
 
-## Manually installing the plugin
+Contributions are welcome! If you would like to contribute to the project, please follow the [contribution guidelines](CONTRIBUTING.md).
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+## License
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
+This plugin is licensed under the [MIT License](LICENSE).
 
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
--- main.ts
+++ main.ts
@@ -1,28 +1,25 @@
 import { 
-	App, 
-	Editor, 
-	MarkdownView, 
-	Modal, 
-	Notice, 
-	Plugin, 
-	PluginSettingTab, 
-	Setting 
+	Plugin
 } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-// storing information
+// ---------------------------- Storing Information ----------------------------
 // This plugin will store a single string
-interface MyPluginSettings {
+interface EditorWidthSliderSettings {
 	sliderPercentage: string;
 }
 // the default value of the thing you want to store 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: EditorWidthSliderSettings = {
 	sliderPercentage: '50'
 }
+// ---------------------------- Storing Information ----------------------------
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+
+// ---------------------------- Plugin Class -----------------------------------
+export default class EditorWidthSlider extends Plugin {
+	settings: EditorWidthSliderSettings;
 
 	// most important function, this gets executed everytime the plugin is first 
 	// loaded, e.g. when obsidian starts, or when the user just installed the 
@@ -32,109 +29,7 @@ export default class MyPlugin extends Plugin {
 		
 		this.addStyle();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			'dice', 
-			'Sample Plugin', 
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-
-		// ----------------------- SLIDER -----------------------
-
-		// Create the slider element
-		const slider = document.createElement('input');
-		slider.type = 'range';
-		slider.min = '0';
-		slider.max = '100';
-		slider.value = this.settings.sliderPercentage;
-		// Apply custom CSS styles to the slider
-		slider.style.width = '150px'; // Adjust the width value as needed
-		// Apply the color to the slider
-		// slider.style.background = '#a882ff';
-		// slider.style.setProperty('color', '#a882ff');
-
-		// Create the text element for displaying the slider value
-		const sliderValueText = document.createElement('span');
-		sliderValueText.textContent = slider.value;
-		sliderValueText.style.marginLeft = '5px';
-		// Apply the color to the text element
-		sliderValueText.style.color = '#a882ff';
-
-		// Add event listener to the slider
-		slider.addEventListener('input', (event) => {
-			const value = parseInt(slider.value);
-			// const widthInPixels = 400 + value * 10;
-			this.settings.sliderPercentage = value.toString();
-
-			this.saveSettings();
-			this.updateStyle();
-			sliderValueText.textContent = value.toString();
-			console.log('Slider value:', value);
-			// Perform any actions based on the slider value
-		});
-
-		// Create the status bar item
-		const statusBarItemEl = this.addStatusBarItem();
-
-		// Append the slider to the status bar item
-		statusBarItemEl.appendChild(slider);
-		statusBarItemEl.appendChild(sliderValueText);
-		// ----------------------- SLIDER -----------------------
-
-
-
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// This adds an editor command that can perform some operation on the 
-		// current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// // This adds a complex command that can check whether the current state 
-		// // of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(
-		// 			MarkdownView
-		// 		);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the 
-		// 			// command can be run.
-		// 			// If checking is false, then we want to actually perform 
-		// 			// the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when 
-		// 			// the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-		// // This adds a settings tab so the user can configure various aspects of 
-		// // the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.createSlider();
 
 		// If the plugin hooks up any global DOM events (on parts of the app 
 		// that doesn't belong to this plugin)
@@ -155,27 +50,69 @@ export default class MyPlugin extends Plugin {
 	}
 
 
+	// ---------------------------- SLIDER -------------------------------------
+	createSlider() {
+
+		// Create the slider element
+		const slider = document.createElement('input');
+		slider.classList.add('editor-width-slider');
+		slider.type = 'range';
+		slider.min = '0';
+		slider.max = '100';
+		slider.value = this.settings.sliderPercentage;
+		// Adjust the width value as needed
+		slider.style.width = '150px'; 
+		
+		// Add event listener to the slider
+		slider.addEventListener('input', (event) => {
+			const value = parseInt(slider.value);
+			// const widthInPixels = 400 + value * 10;
+			this.settings.sliderPercentage = value.toString();
+
+			this.saveSettings();
+			this.updateEditorStyle();
+			sliderValueText.textContent = value.toString();
+			console.log('Slider value:', value);
+			// Perform any actions based on the slider value
+		});
+
+		// Create the text element for displaying the slider value
+		const sliderValueText = document.createElement('span');
+		sliderValueText.textContent = slider.value;
+		sliderValueText.classList.add('editor-width-slider-value');
+		sliderValueText.style.marginLeft = '5px';
+
+
+		// Create the status bar item
+		const statusBarItemEl = this.addStatusBarItem();
+		// Append the slider to the status bar item
+		statusBarItemEl.appendChild(slider);
+		statusBarItemEl.appendChild(sliderValueText);
+	}
+	// ---------------------------- SLIDER -------------------------------------
+
+
 
 	// add the styling elements we need
 	addStyle() {
 		// add a css block for our settings-dependent styles
 		const css = document.createElement('style');
-		css.id = 'additional-css';
+		css.id = 'additional-editor-css';
 		document.getElementsByTagName("head")[0].appendChild(css);
 
 		// add the main class
-		document.body.classList.add('additional-css');
+		document.body.classList.add('additional-editor-css');
 
 		// update the style with the settings-dependent styles
-		this.updateStyle();
+		this.updateEditorStyle();
 	}
 
 	
 	// update the styles (at the start, or as the result of a settings change)
-	updateStyle() {
+	updateEditorStyle() {
 		// get the custom css element
-		const el = document.getElementById('additional-css');
-		if (!el) throw "additional-css element not found!";
+		const el = document.getElementById('additional-editor-css');
+		if (!el) throw "additional-editor-css element not found!";
 		else {
 
 		el.innerText = `
@@ -186,7 +123,7 @@ export default class MyPlugin extends Plugin {
 		}
 	}
 
-	// Method to store settings
+	// Method to load settings
 	async loadSettings() {
 		this.settings = Object.assign(
 			{}, 
@@ -195,54 +132,10 @@ export default class MyPlugin extends Plugin {
 		);
 	}
 
-	// Method to load settings
+	// Method to store settings
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
-
-// class SampleSettingTab extends PluginSettingTab {
-// 	plugin: MyPlugin;
-
-// 	constructor(app: App, plugin: MyPlugin) {
-// 		super(app, plugin);
-// 		this.plugin = plugin;
-// 	}
-
-// 	display(): void {
-// 		const {containerEl} = this;
-
-// 		containerEl.empty();
-
-// 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-// 		new Setting(containerEl)
-// 			.setName('Setting #1')
-// 			.setDesc('It\'s a secret')
-// 			.addText(text => text
-// 				.setPlaceholder('Enter your secret')
-// 				.setValue(this.plugin.settings.mySetting)
-// 				.onChange(async (value) => {
-// 					console.log('Secret: ' + value);
-// 					this.plugin.settings.mySetting = value;
-// 					await this.plugin.saveSettings();
-// 				}));
-// 	}
-// }
+// ---------------------------- Plugin Class -----------------------------------
