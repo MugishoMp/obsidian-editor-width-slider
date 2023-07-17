ca2d48000d106b25de7c1425be9c62a55d4366e9
mugisho <mugishompozi@gmail.com>
2023-07-17 20:55:23 +0200

    Clean up code

--- main.ts
+++ main.ts
@@ -2,8 +2,6 @@ import {
 	Plugin
 } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
 // ---------------------------- Storing Information ----------------------------
 // This plugin will store a single string
 interface EditorWidthSliderSettings {
@@ -30,19 +28,6 @@ export default class EditorWidthSlider extends Plugin {
 		this.addStyle();
 
 		this.createSlider();
-
-		// If the plugin hooks up any global DOM events (on parts of the app 
-		// that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when 
-		// this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear 
-		// the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => 
-			console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
